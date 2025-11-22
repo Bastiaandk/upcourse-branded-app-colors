99973e365fd4ad8ea46275318893747bd40687af
Bastiaandk <132281435+Bastiaandk@users.noreply.github.com>
2025-11-22 16:45:27 +0100

    Update upcourse-colorswitch.js

--- upcourse-colorswitch.js
+++ upcourse-colorswitch.js
@@ -131,7 +131,26 @@
     TOGGLE UI HTML
     -------------------------------------------- */
 
+    function injectToggleHTML(forcedMode) {
+        const bar = document.createElement('div');
+        bar.id = 'jiffy_toggle_bar';
 
+        const emoji = forcedMode === 'dark' ? '☾' : '𖤓';
+
+        bar.innerHTML = `
+            <div id="jiffy_toggle_inner">
+                <span id="emoji_left" style="font-size:22px;">debug v2 - 🎨</span>
+
+                <div id="jiffy_switch" aria-role="switch">
+                    <div class="slider"></div>
+                </div>
+
+                <span id="emoji_right" style="font-size:22px;">${emoji}</span>
+            </div>
+        `;
+
+        return bar;
+    }
 
     /* --------------------------------------------
     INIT TOGGLE BAR
